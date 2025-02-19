053cea72dfbb12ec5d0f57294a5b0f8aa64e8418
Md Umar Faruk <mdumarfaruk793@gmail.com>
2025-02-20 00:15:38 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,13 @@
+//Relational (Comparison) Operators
+class RelationalOperators{
+    public static void main(String[] args) {
+int a = 13, b = 20;RelationalOperators
+System.out.println(a == b); // false
+System.out.println(a != b); // true
+System.out.println(a > b);  // true
+System.out.println(a < b);  // false
+System.out.println(a >= b); // true
+System.out.println(a <= b); // false
+
+}
+}
